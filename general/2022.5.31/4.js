93c80bcf4e8c295a4d7211150757acf76bfbeba2
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-31 22:16:06 -0400

    Create 4.js

---
+++ general/2022.5.31/4.js
@@ -0,0 +1,25 @@
+/* Parameters:
+bucket(ary, fn)
+ary - an array
+fn - a function that returns true or false
+fn has one argument, a value to test
+returns:
+an array containing two arrays:
+one with all elements that passed the test, fn
+the other with all elements that did not pass the test, fn */
+
+function bucket(ary, fn)
+{
+	return ary.reduce
+	(
+		function(ret, cur)
+		{
+			fn(cur) ? ret[0].push(cur) : ret[1].push(cur);
+			return ret;
+		}, [[], []]
+	);
+}
+
+num = [1, 2, 3, 4, 5, 6];
+[evens, odds] = bucket(num, n => n % 2 == 0);
+console.log("evens", evens, "| odds", odds);	
