e49942b2fbf0b6a82b4140a5dabd1ba336113728
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-31 20:30:30 -0400

    Update 2.js

--- general/2022.5.31/2.js
+++ general/2022.5.31/2.js
@@ -1,4 +1,4 @@
-/*  call_fn(fn, n arg)
+/* call_fn(fn, n arg)
 fn - the function to be called repeatedly
 n - the number of times to call function, fn
 arg - the argument to pass to function, fn, when it is called */
