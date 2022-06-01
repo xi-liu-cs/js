d3de5af233e2392d7f3127a029d458fc36b27fc6
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-31 20:58:59 -0400

    Create 3.js

---
+++ general/2022.5.31/3.js
@@ -0,0 +1,4 @@
+/* betterCallFn(fn, n, arg1, ..., argn)
+fn - the function to be called repeatedly
+n - the number of times to call function, fn
+arg1, ..., argn - all of the arguments to pass in to the function fn when it is called */
