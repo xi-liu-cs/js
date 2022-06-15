9dc0cf2e882a767b9caee98949e6f91cb3f07b32
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-06-15 14:27:28 -0400

    Update 6.js

--- general/applied internet technology, hw2/6.js
+++ general/applied internet technology, hw2/6.js
@@ -1,10 +1,23 @@
+import { readFile } from 'node:fs';
+
 function my_read_file(file_name, success_fn, error_fn)
 {
-    fs.readFile(file_name, 'utf-8', (err, data)) =>
-    {
+    fs.readFile(file_name, 'utf-8', (err, data) => {
         if(!err)
             success_fn(data);
         else
             error_fn(err);
-    }
+    });
 }
+
+function success_fn()
+{
+    console.log("success_fn");
+}
+
+function error_fn()
+{
+    console.log("error_fn");
+}
+
+my_read_file('a.txt', success_fn, error_fn);
